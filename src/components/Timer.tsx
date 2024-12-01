53ee4f6bcf451ba725e3457db6c26503a45b404d
T_Mori <morit0301@gmail.com>
2024-12-01 17:50:00 +0900

    タイマー機能の実装

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,19 +1,67 @@
 import { Pause, Play, RefreshCcw } from "lucide-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ControlButton } from "./ControlButton";
 import { ModeTypeButton } from "./ModeTypeButton";
+import { TIMER_OPTIONS } from "../constants";
 
 const Timer = () => {
   const [mode, setMode] = useState<"work" | "break">("work");
   const [isRunning, setIsRunning] = useState(false);
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [now, setNow] = useState<number | null>(null);
+  const [pausedTimeRemaining, setPausedTimeRemaining] = useState<number | null>(
+    null
+  );
 
-  // ダミーコード
-  const displayMinutes = "25";
-  const displaySeconds = "00";
-  const handleChangeMode = () => {};
-  const handleStart = () => {};
-  const handleStop = () => {};
-  const handleReset = () => {};
+  const intervalRef = useRef<NodeJS.Timeout | undefined>(undefined);
+
+  function handleStart() {
+    // 後にAudio の設定を追加
+    const currentTime = Date.now();
+    if (!isRunning && pausedTimeRemaining) {
+      setStartTime(currentTime - pausedTimeRemaining);
+      setPausedTimeRemaining(null);
+    } else {
+      setStartTime(currentTime);
+    }
+    setNow(Date.now());
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 1000);
+
+    setIsRunning(true);
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
+    setIsRunning(false);
+    if (startTime != null && now != null) {
+      setPausedTimeRemaining(now - startTime);
+    }
+  }
+
+  function handleReset() {
+    clearInterval(intervalRef.current);
+    setStartTime(null);
+    setNow(null);
+    setIsRunning(false);
+    setPausedTimeRemaining(null);
+  }
+
+  function handleChangeMode() {
+    handleReset();
+    setMode(mode === "work" ? "break" : "work");
+  }
+
+  // 経過時間(秒)
+  const secondsPassed =
+    startTime != null && now != null ? Math.floor((now - startTime) / 1000) : 0;
+  // 残り時間の秒数（設定時間ー経過時間）
+  const calculateTime = TIMER_OPTIONS[mode].minutes * 60 - secondsPassed;
+  const displayMinutes = Math.floor(calculateTime / 60);
+  const displaySeconds =
+    calculateTime % 60 < 10 ? "0" + (calculateTime % 60) : calculateTime % 60;
 
   return (
     <div
